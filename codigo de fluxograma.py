1c01b463de46921d1c5899d197eacc4a806a3828
AlexandreJefersonPereiradaSilva <dairodrigues281@gmail.com>
2025-05-12 15:57:40 -0300

    codigo de fluxograma e else media do aluno

---
+++ codigo de fluxograma.py
@@ -0,0 +1,44 @@
+
+saldo=int(input("digite o valor bancário"))
+saque=int(input("digite o valor de saque"))
+
+if saldo>=saque:
+    saldo=saldo-saque
+    print("O saque foi realizado com sucesso! Saia de sua conta.")
+
+else:
+   print("Não possui saldo suficiente para realizar essa operação:", saldo)
+
+
+nota1=int(input("digite a primeira nota"))
+nota2=int(input("digite a segunda nota"))
+media=(nota1+nota2)/2 
+
+if media>=6:
+    print("aprovado")
+
+else:
+    print("reprovado")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
