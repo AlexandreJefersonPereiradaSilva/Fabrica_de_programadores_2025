d229f6cdb8070e2270f65f47213499b24b1a346e
AlexandreJefersonPereiradaSilva <dairodrigues281@gmail.com>
2025-05-15 15:21:33 -0300

    Add files via upload

---
+++ biblioteca_random.py
@@ -0,0 +1,14 @@
+
+import random
+defensores=('Demolidor', 'Jessica Jones', 'Luke Cage', 'Punho de ferro.')
+
+print(random.choice(defensores))
+
+import time
+
+print(time.asctime())
+
+import turtle
+
+turtle.circle(50)
+turtle.getscreen((reversed)._root.mainloop)
