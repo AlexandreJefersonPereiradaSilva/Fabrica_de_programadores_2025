db07b48ba2aaa79efca26a08164787468501a31b
AlexandreJefersonPereiradaSilva <dairodrigues281@gmail.com>
2025-05-23 14:51:50 -0300

    Curso Python

---
+++ EXERCICIOS_WHILE.py
@@ -0,0 +1,33 @@
+'''#exercicio5.1
+numero=1
+while numero<=100:
+    print(numero)
+    numero=numero+1
+
+#exercicio5.2
+numero=50
+while numero<=100:
+    print(numero)
+    numero=numero+1
+'''
+#exercicio5.3
+contagem_regressiva=10
+while contagem_regressiva>=0:
+    print(contagem_regressiva)
+    contagem_regressiva=contagem_regressiva-1
+print("fogo")
+
+#imprimindo numeros pares
+fim=int(input("digite o ultimo numero a imprimir:"))
+x = 0
+while x <= fim:
+    if x % 2 == 1:
+        print(x," É numero impar")
+
+    x=x+1
+
+
+
+
+    
+
