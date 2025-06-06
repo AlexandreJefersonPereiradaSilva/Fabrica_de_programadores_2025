7e3570cb302c48a519774f6775335b64cdd60924
AlexandreJefersonPereiradaSilva <dairodrigues281@gmail.com>
2025-06-06 14:09:56 -0300

    Curso Python

---
+++ pizzaria.py
@@ -0,0 +1,21 @@
+input("")
+valor1= "muçarela 40,50"
+valor2="toscana 39,99"
+valor3= "marguerita 40,99"
+valor4= "calabresa 42,50"
+
+sabor1 = "muçarela"
+sabor2 = "toscana"
+sabor3 = "marguerita"
+sabor4 = "muçarela"
+
+quantidade=int(input("Digite quantas pizzas voce ira pedir"))
+nome_do_cliente=int(input("digite seu nome"))
+whatsApp=int(input("digite seu numero de whatsApp"))
+
+
+from datetime import date
+data_atual = date.today
+print(data_atual)
+
+
