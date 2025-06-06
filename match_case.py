c28332a9283e28878116046a1ef55d4bdc3c5ff3
AlexandreJefersonPereiradaSilva <dairodrigues281@gmail.com>
2025-06-06 14:10:31 -0300

    Curso Python

---
+++ match_case.py
@@ -0,0 +1,12 @@
+import random
+lista = random.randint(1,2,3,4)
+match lista:
+    case 1:
+        print("O homem sem medo!")
+    case 2:
+        print("matadora de aluguel!")
+    case 3:
+        print("O poderoso!")
+    case 4:
+        print("PUnhos da liberdade!")
+     
