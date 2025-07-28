804df20fa40944444c1b912baf74e594efdab185
AlexandreJefersonPereiradaSilva <dairodrigues281@gmail.com>
2025-07-28 15:23:24 -0300

    Curso Python

---
+++ RESUMO_PY.py
@@ -0,0 +1,70 @@
+A = 3
+B = 2
+C = 4
+D = 10
+
+#1 função
+def funçao():
+    
+    print(A==B)
+    print(C>D)
+    print(A<C)
+    print(D>=B)
+    print(A<=C)
+    print(C==C)
+    print(B<C)
+    print(A!=D)
+    print(D>B)
+    print(A<=B)
+
+funçao()
+
+#operaçoes logicas
+def logicas():
+    print("operações lógicas")
+    print(A==B or A<C)
+    print(C>D)
+    print(A<C)
+    print(D>=B)
+    print(A!=D or C>=B)
+    print(C>=B or A==B)
+    print(D!=A or D>A)
+    print(A==C or B<D)
+    print(C==C or D>=B)
+
+logicas()
+
+
+#operaçoes relacionais
+def relacionais():
+    print("operações relacionais")
+    print(A==B)
+    print(C>D)
+    print(A<C)
+    print(D>=B)
+    print(A<=C)
+    print(C==C)
+    print(B<C)
+    print(A!=D)
+    print(D>B)
+    print(A<=B)
+
+relacionais()
+
+
+
+#operações aritmeticas
+def aritméticas():
+    print("operações aritmétricas")
+    print(A**B)
+    print(C+A-D)
+    print(B**D-A/C)
+    print(A+C)
+    print(C-D)
+    print(A/C)
+    print(B+A+D)
+    print(A-D**C)
+    print(D+C**A)
+    print(B-C/A)
+
+aritméticas()
