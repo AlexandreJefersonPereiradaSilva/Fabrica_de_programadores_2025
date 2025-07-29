a0109c15b4eae6ea826b4a77e846a324459ee04b
AlexandreJefersonPereiradaSilva <dairodrigues281@gmail.com>
2025-07-29 15:53:47 -0300

    Curso Python

---
+++ automaçao.py
@@ -0,0 +1,25 @@
+import time
+import pyautogui as fs1
+'''
+fs1.mouseInfo()
+fs1.moveTo(1800,23)
+fs1.sleep(1)
+fs1.click()
+'''
+
+'''
+fs1.mouseInfo()
+fs1.moveTo(1879,11)
+fs1.sleep(1)
+fs1.click()
+'''
+
+fs1.moveTo(610,1046,duration=2)
+fs1.click()
+fs1.sleep(1)
+fs1.write("hola amigos")
+
+
+
+
+
